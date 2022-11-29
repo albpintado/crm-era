5134c5b517592d9f19dff18d47705d52202188aa
albpintado <albpintado@protonmail.ch>
2022-11-29 13:17:57 +0100

    Remove id autogenerated and password hashed from asserts

--- src/test/java/com/albpintado/crmera/user/UserServiceTests.java
+++ src/test/java/com/albpintado/crmera/user/UserServiceTests.java
@@ -8,6 +8,7 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.util.Optional;
 
@@ -76,9 +77,7 @@ public class UserServiceTests {
     ResponseEntity<User> actualResponse = this.service.create(userDto);
 
     assertThat(actualResponse.getStatusCode().value(), equalTo(201));
-    assertThat(actualResponse.getBody().getId(), is(expectedUser.getId()));
     assertThat(actualResponse.getBody().getName(), is(expectedUser.getName()));
     assertThat(actualResponse.getBody().getEmail(), is(expectedUser.getEmail()));
-    assertThat(actualResponse.getBody().getPassword(), is(expectedUser.getPassword()));
   }
 }
