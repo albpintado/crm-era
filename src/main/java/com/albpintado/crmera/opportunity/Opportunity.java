3ea66b16d3147f317d192144526c06c2c9d7a9bf
Pintado <alberto.pintado@solera.com>
2022-11-29 09:40:12 +0100

    Create customer and opportunity entities

---
+++ src/main/java/com/albpintado/crmera/opportunity/Opportunity.java
@@ -0,0 +1,82 @@
+package com.albpintado.crmera.opportunity;
+
+import com.albpintado.crmera.customer.Customer;
+import org.hibernate.Hibernate;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "opportunity")
+public class Opportunity {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  @Column(name = "id", nullable = false)
+  private Long id;
+
+  @Column(name = "name", nullable = false)
+  private String name;
+
+  @Column(name = "phone", nullable = false)
+  private String phone;
+
+  @Column(name = "email", nullable = false)
+  private String email;
+
+  @ManyToOne
+  @JoinColumn(name = "customer_id")
+  private Customer customer;
+
+  public Customer getCustomer() {
+    return customer;
+  }
+
+  public void setCustomer(Customer customer) {
+    this.customer = customer;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPhone() {
+    return phone;
+  }
+
+  public void setPhone(String phone) {
+    this.phone = phone;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+    Opportunity that = (Opportunity) o;
+    return id != null && Objects.equals(id, that.id);
+  }
+
+  @Override
+  public int hashCode() {
+    return getClass().hashCode();
+  }
+}
---
+++ src/main/java/com/albpintado/crmera/opportunity/OpportunityRepository.java
@@ -0,0 +1,6 @@
+package com.albpintado.crmera.opportunity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OpportunityRepository extends JpaRepository<Opportunity, Long> {
+}
