e1bdc2340f51cf41083975726cd8a29e22d08a0a
albpintado <albpintado@protonmail.ch>
2022-11-29 12:59:16 +0100

    Add User DTOs

---
+++ src/main/java/com/albpintado/crmera/user/UserEmailDto.java
@@ -0,0 +1,13 @@
+package com.albpintado.crmera.user;
+
+public class UserEmailDto {
+  private String email;
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+}
