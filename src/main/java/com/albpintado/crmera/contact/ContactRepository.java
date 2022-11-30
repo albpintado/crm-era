773c81d53aa2a61cc1ca741879220f916f3d3390
albpintado <albpintado@protonmail.ch>
2022-11-30 20:11:53 +0100

    Add ContactRepository findByOpportunity_Id method

--- src/main/java/com/albpintado/crmera/contact/ContactRepository.java
+++ src/main/java/com/albpintado/crmera/contact/ContactRepository.java
@@ -2,5 +2,8 @@ package com.albpintado.crmera.contact;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ContactRepository extends JpaRepository<Contact, Long> {
+  List<Contact> findByOpportunity_Id(Long id);
 }
